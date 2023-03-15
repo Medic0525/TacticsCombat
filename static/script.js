b5ab5c99e7fb8674fe902ba27c7afbe109bbe53d
minstrike520 <minstrike520@gmail.com>
2023-03-15 15:59:06 +0800

    added sprite, space and data.get method

--- static/script.js
+++ static/script.js
@@ -1,34 +1,78 @@
+const url = "http://localhost:81/";
+
+let datax;
+$.get(url+"/data", function(data){
+    console.log("data:",data)
+    datax = data;
+    /*
+    Sprite.pos = data["sprite.pos"];
+    Space.grids = data["space.grids"];
+    Space.relativePos = data["space.relativePos"]
+    */
+    
+});
+
+const Sprite = {
+    pos: [0,0],
+    draw: function (ctx) {
+        ctx.fillStyle = "red";
+        ctx.fillRect(this.pos[0], this.pos[1], 50, 50);
+    }
+}
+const Space = {
+    sideLength: 50,
+    relativePos:{
+        x: 0, y: 0
+    },
+    tileDict: {
+        0: "green",
+        1: "black"
+    },
+    draw: function(ctx) {
+        console.log("grids:",this.grids);
+        let gridX = 0; let gridY = 0;
+        for (grid_line of this.grids) {
+            for (let grid of grid_line) {
+                ctx.fillStyle = tileDict[grid];
+                ctx.fillRect(
+                    this.relativePos.x + gridX*this.sideLength,
+                    this.relativePos.y + gridY*this.sideLength,
+                    this.sideLength,
+                    this.sideLength)
+                gridX += 1;
+            }
+            gridY += 1;
+        }
+    }
+}
+
+
+
 $(function () {
     // Socket.IO Start connect
-    var socket = io.connect();
-    
+    var socket = io.connect(); 
     // Socket.IO send message
     $("#send").click(function (e) {
         // Send message
         socket.emit('send', $('#message').val())
         // Clear input field
         $('#message').val('')
-    });
-    
+    }); 
     // Socket.IO get message
     socket.on('get', function (data) {
         $('#chat_content').append('<p>I say: ' + data + '</p>');
     });
-    
     // Socket.IO get test
     socket.on("message", function (data) {
         $('#chat_content').append('<p>System : ' + data + '</p>');
-    });
-    
+    }); 
     // Socket.IO send test
     $("#test").click(function (e) {
         socket.emit('test')
     });
-
     //sprite moving by keyboard controlling
-    socket.on("spritemove", function (sp) {
-        spriteposition = sp;
-    });
+    
+    
 
 
     var canvas = document.querySelector("canvas");
@@ -39,23 +83,33 @@ $(function () {
     // and one grid is 40p
 
     ctx.fillStyle = "red";
-    ctx.fillRect(50, 50, 50, 50)
+    ctx.fillRect(50, 50, 50, 50);
 
-    let spriteposition = [];
-
-    function drawSprite(ctx, sp){
-        ctx.fillStyle = "red";
-        ctx.fillRect(sp[0], sp[1], 50, 50);
-    }
-    drawSprite(ctx, spriteposition);
 
     
 
+    
+    //update
+    socket.on("sprite.pos", function (spriteposition) {
+        Sprite.pos = spriteposition; //[]
+    });
+    socket.on("space.grids", function (grids) {
+        Space.grids = grids; //[[]]
+    });
+    socket.on("space.relativePos", function (relativePos){
+        Space.relativePos = relativePos; // []
+    });
+    $
+
+
+    Space.draw(ctx);
+    Sprite.draw(ctx);
+
+    
+    
+
     let keystatus = {
-        "d": false,
-        'a': false,
-        "w": false,
-        's': false
+        "d": false, 'a': false, "w": false, 's': false
     }
 
     /*
@@ -86,7 +140,8 @@ $(function () {
         window.requestAnimationFrame(animate);
         ctx.clearRect(0,0,canvas.width, canvas.height);
         socket.emit("key", keystatus);
-        drawSprite(ctx, spriteposition);
+        Sprite.draw(ctx);
+        Space.draw(ctx);
         //console.log(spriteposition);
     }
     animate();
