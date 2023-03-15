23286d346192ec0e9174045c5221aa114d8f6488
Medic0525 <Ryanandanny@gmail.com>
2023-03-15 16:40:27 +0800

    ur gay, here's da file added items.py

---
+++ static/items.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+import random
+import flask
+
+
+Random = random.SystemRandom()
+
+@dataclass
+class user:
+    userid: int = field(init = False)
+    username: str = field(init = False)
+    nickname: str
+    hashedpassword: int = field(init = False)
+    accountmail: str
+    
+
+    def __post_init__(self, password, accountname):
+        nameid = str(Random.randint(0,9999)).zfill(4)
+        self.hashedpassword = hash(password)
+        self.username = F"{accountname}#{nameid}"
