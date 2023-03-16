5d933c131716d79cfa59ff0683659a282437abb7
Medic0525 <Ryanandanny@gmail.com>
2023-03-16 14:08:26 +0800

    added active ability and kit

--- static/items.py
+++ static/items.py
@@ -9,6 +9,7 @@ Random = random.SystemRandom()
 class user:
     userid: int = field(init = False)
     username: str = field(init = False)
+    profile_picture: str = field(init = False)
     nickname: str
     hashedpassword: int = field(init = False)
     accountmail: str
@@ -19,3 +20,33 @@ class user:
         self.hashedpassword = hash(password)
         self.username = F"{accountname}#{nameid}"
 
+@dataclass
+class active_ability:
+    damage: float = field(init = False)
+    enchantment = dict
+    healing: float = field(init = False)
+
+        def __post_init__(self):
+            pass
+
+@dataclass
+class kit:
+    player_name: str
+    icon: str
+    hp: int
+    mp: int
+    attack: int
+    defense: int
+    enchantment: dict
+    active_skill_1: active_ability
+    active_skill_2: active_ability
+    active_skill_3: active_ability
+    active_skill_4: active_ability
+    active_skill_5: active_ability
+
+
+def main():
+    print("item testing")
+
+if __name__ == "__main__":
+    main()
