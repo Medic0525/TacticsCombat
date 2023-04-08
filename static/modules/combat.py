5708aadc74c47cdcdc47631c8a3036ba79592ab1
minstrike520 <minstrike520@gmail.com>
2023-04-08 14:44:47 +0800

    Moved calculations in combat.py to calculations.py

--- static/modules/combat.py
+++ static/modules/combat.py
@@ -1,26 +1,4 @@
-#region Functions
-def distance(start: list, end: list):
-    return end[0]-start[0]+end[1]-start[1]
-
-def is_legal_move(start: list, end: list, maxstep: int):
-    if distance(start, end) <= maxstep:
-        return True
-    return False
-
-
-
-def attack_counter(attacker: dict, defender: dict):
-    return attacker["inborn"]["atk"]-defender["inborn"]["dfc"]
-
-def skillselector(skill, attacker, *defender):
-    defender = defender[0] if defender else 0
-    match skill:
-        case "attack":
-            dmg = attack_counter(attacker,defender)
-            defender["hp"] -= dmg
-        case "magic":
-            pass
-#endregion
+from calculations import *
 
 class Player:
     def __init__(self, name: str, pos: list, hp: int,
