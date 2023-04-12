b10b590347fd3dcbabcae594ee6bc44f98796405
Medic0525 <Ryanandanny@gmail.com>
2023-04-12 15:44:20 +0800

    finally fixed login system

--- static/modules/backend/login.py
+++ static/modules/backend/login.py
@@ -28,63 +28,32 @@ class User:
         file["player_count"] += 1
         json.dumps(file)
 
-        self.password = hash(password)
+        self.password = int(hash(password))
 
         self.account_mail = account_mail
         
         cur.execute(F"INSERT INTO user VALUES('{self.userid}', '{self.profile_name}', '{self.username}', '{self.profile_picture}', '{self.nickname}', '{self.password}', '{self.account_mail}')")
         con.commit()
 
-    def login(account_mail, password):
-        log_password = con.execute(F"SELECT password FROM user WHERE profile_name='{account_mail}', CASE WHEN password IS NULL THEN 0 ELSE password")
-        if password == log_password:
+    def login(self, account_mail, password):
+        log_password = con.execute(F"SELECT password FROM user WHERE account_mail='{account_mail}'").fetchone()
+        print(log_password[0])
+        print(int(hash(password)))
+        if log_password is not None and int(hash(password)) is log_password[0]:
             return True
         else:
             return False
 
 
-
-"""
-class Position:
-    def __init__(self, x, y):
-        self.x = x
-        self.y =y
-
-class Player:
-    def __init__(self, icon: str, hp: int, mp: int, attack: int, defense: int, movement: int, pos: Position, enchantment: dict = dict(),  skills: list = list()):
-        self.icon = icon
-        self.hp = hp
-        self.mp = mp
-        self.attack = attack
-        self.defense = defense
-        self.movement = movement
-
-        def get_pos():
-            return self.pos
-
-        def set_pos(self, x, y):
-            self.pos.x = x
-            self.pos.y = y
-
-        def move(self, new_pos):
-            x = new_pos.x - self.pos.x
-            y = new_pos.y - self.pos.y
-
-            if x+y > movement:
-                return False
-            else:
-                return (x,y)    
-"""    
-
-
-
-
 def main():
 
-    testing_user = User(username = "Medic0525", password = 123, account_mail = "minstrike520@gmail.com")
+    testing_user = User(username = "Medic0525", password = "123", account_mail = "minstrike520@gmail.com")
     test = cur.execute("SELECT * FROM user WHERE profile_name='Medic0525'")
     con.commit()
     print(test.fetchone())
+    username = input("input email")
+    password = input("input password")
+    print(testing_user.login(username, password))
     cur.execute("DELETE FROM user WHERE profile_name='Medic0525'")
     con.commit()
     print("register system testing")
