d7c10c9c1151628230388a50787a97317fc59899
minstrike520 <minstrike520@gmail.com>
2023-04-12 13:47:45 +0800

    Merging @Medic0525 's login system

--- static/modules/backend/items.py
+++ static/modules/backend/items.py
@@ -1,52 +1,93 @@
-from dataclasses import dataclass, field
 import random
-
-
+import sqlite3
+import json
 
 Random = random.SystemRandom()
+con = sqlite3.connect("combat.db")
+cur = con.cursor()
 
-@dataclass
-class user:
-    userid: int = field(init = False)
-    username: str = field(init = False)
-    profile_picture: str = field(init = False)
-    nickname: str
-    hashedpassword: int = field(init = False)
-    accountmail: str
-    
+cur.execute("CREATE TABLE IF NOT EXISTS user(user_id INT, profile_name VARCHAR, username VARCHAR, profile_picture VARCHAR, nickname VARCHAR(20), password VARCHAR, account_mail VARCHAR)")
 
-    def __post_init__(self, password, accountname):
+with open("settings.json") as j:
+    file = json.load(j)
+
+
+class User:
+    def __init__(self, username: str,  password: str, account_mail: str, profile_picture=None, nickname=None):
+        
         nameid = str(Random.randint(0,9999)).zfill(4)
-        self.hashedpassword = hash(password)
-        self.username = F"{accountname}#{nameid}"
+        self.profile_name = username
+        self.username = F"{username}#{nameid}"
 
-@dataclass
-class active_ability:
-    damage: float = field(init = False)
-    enchantment = dict
-    healing: float = field(init = False)
+        self.profile_picture = profile_picture
 
-    def __post_init__(self):
-        pass
+        self.nickname = nickname if nickname != None else username
 
-@dataclass
-class kit:
-    player_name: str
-    icon: str
-    hp: int
-    mp: int
-    attack: int
-    defense: int
-    enchantment: dict
-    active_skill_1: active_ability
-    active_skill_2: active_ability
-    active_skill_3: active_ability
-    active_skill_4: active_ability
-    active_skill_5: active_ability
+        self.userid = str(file["id"]).zfill(9)
+        self.userid = int(self.userid)
+        file["id"] += 1
+        json.dumps(file)
+
+        self.password = hash(password)
+
+        self.account_mail = account_mail
+        
+        cur.execute(F"INSERT INTO user VALUES('{self.userid}', '{self.profile_name}', '{self.username}', '{self.profile_picture}', '{self.nickname}', '{self.password}', '{self.account_mail}')")
+        con.commit()
+
+    def login(account_mail, password):
+        log_password = con.execute(F"SELECT password FROM user WHERE profile_name='{account_mail}', CASE WHEN password IS NULL THEN 0 ELSE password")
+        if password == log_password:
+            return True
+        else:
+            return False
+
+
+
+"""
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y =y
+
+class Player:
+    def __init__(self, icon: str, hp: int, mp: int, attack: int, defense: int, movement: int, pos: Position, enchantment: dict = dict(),  skills: list = list()):
+        self.icon = icon
+        self.hp = hp
+        self.mp = mp
+        self.attack = attack
+        self.defense = defense
+        self.movement = movement
+
+        def get_pos():
+            return self.pos
+
+        def set_pos(self, x, y):
+            self.pos.x = x
+            self.pos.y = y
+
+        def move(self, new_pos):
+            x = new_pos.x - self.pos.x
+            y = new_pos.y - self.pos.y
+
+            if x+y > movement:
+                return False
+            else:
+                return (x,y)    
+"""    
+
+
 
 
 def main():
-    print("item testing")
+
+    testing_user = User(username = "Medic0525", password = 123, account_mail = "minstrike520@gmail.com")
+    test = cur.execute("SELECT * FROM user WHERE profile_name='Medic0525'")
+    con.commit()
+    print(test.fetchone())
+    cur.execute("DELETE FROM user WHERE profile_name='Medic0525'")
+    con.commit()
+    print("register system testing")
 
 if __name__ == "__main__":
     main()
